b85d78f250929e1f3ca61dbea2824331a6a6e7a2
EjuMir <mirejo101@gmail.com>
2024-05-10 23:24:26 +0600

    footer done

--- src/Root/Roots.jsx
+++ src/Root/Roots.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Layout/Navbar/Navbar";
+import Footer from "../Layout/Footer/Footer";
 
 const Roots = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
